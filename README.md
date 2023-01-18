e1be08fbf34ed3fad4f016de59aa3980d4435c4b
shine-lzx <1597395958@qq.com>
2023-01-18 09:14:45 +0800

    update

---
+++ README.md
@@ -0,0 +1,77 @@
+**install**
+
+```javascript
+npm install vite-plugin-upload-server
+or
+yarn add vite-plugin-upload-server
+```
+
+**Use**
+
+````javascript
+// vite.config.ts
+
+/**
+ * @host 服务器地址
+ * @username 用户名
+ * @password 密码
+ * @filePath 服务器上项目的目录
+ */
+
+/**
+ * @command
+ * @mode
+ * @server
+ * @build
+ * @cleanFiles 是否需要清理目录
+ */
+
+const server = {
+  development: {
+    host: '',
+    username: '',
+    password: '',
+    filePath: '',
+  },
+  test: {
+    host: '',
+    username: '',
+    password: '',
+    filePath: '',
+  },
+  production: {
+    host: '',
+    username: '',
+    password: '',
+    filePath: '',
+  },
+}
+
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      uploadServerPlugin({
+        command,
+        mode,
+        build: './dist',
+        server,
+      }),
+    ],
+  }
+})
+
+// package.json
+```json
+"scripts": {
+"start": "vite",
+"preview": "vite preview",
+"build:dev": "tsc && vite build --mode development",
+"build:test": "tsc && vite build --mode test",
+"build:prod": "tsc && vite build --mode production"
+},
+
+````
+
+```
+
+```
---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import { NodeSSH } from 'node-ssh'
+import type { ConfigEnv } from 'vite'
+import pc from 'picocolors'
+
+export type Server = {
+  development: Content
+  test: Content
+  production: Content
+}
+
+export type Content = {
+  host: string
+  username: string
+  password: string
+  filePath: string
+}
+
+export interface Options extends ConfigEnv {
+  build: string
+  server: Server
+  cleanFiles?: boolean
+}
+
+export type Env = 'development' | 'test' | 'production'
+
+class FileHandler {
+  ssh: NodeSSH
+  server: Content
+  distPath: string
+  cleanFiles: boolean | undefined
+  constructor({ mode, server, build, cleanFiles }: Options) {
+    this.ssh = new NodeSSH()
+    this.server = server[mode as Env]
+    this.distPath = build
+    this.cleanFiles = cleanFiles
+    this.apply()
+  }
+
+  async apply() {
+    await this.connectServer()
+    const serverDir = this.server.filePath
+    if (this.cleanFiles) {
+      console.log(pc.yellow('执行清理原目录文件……'))
+      await this.ssh.execCommand(`rm -rf ${serverDir}/*`)
+      console.log(pc.green('执行完成'))
+    }
+    console.log(pc.blue('开始上传文件'))
+    await this.uploadFiles(this.distPath, serverDir)
+    this.ssh.dispose()
+  }
+
+  async connectServer() {
+    await this.ssh.connect({
+      host: this.server.host,
+      username: this.server.username,
+      password: this.server.password,
+    })
+    console.log(pc.green('服务器连接成功'))
+  }
+
+  async uploadFiles(localPath: string, remotePath: string) {
+    const status = await this.ssh.putDirectory(localPath, remotePath, {
+      recursive: true,
+      concurrency: 10,
+    })
+
+    console.log(status ? pc.green('文件上传成功') : pc.red('文件上传失败'))
+  }
+}
+
+const uploadServerPlugin = ({
+  command,
+  mode,
+  server,
+  build = './dist',
+  cleanFiles = false,
+}: Options) => {
+  return {
+    name: 'vite-plugin-upload-server',
+    closeBundle() {
+      if (command === 'build') {
+        if (!build) throw 'please input build'
+        new FileHandler({ command, mode, server, build, cleanFiles })
+        return Promise.resolve()
+      }
+    },
+  }
+}
+
+export default uploadServerPlugin
