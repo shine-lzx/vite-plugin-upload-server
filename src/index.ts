561caa9ebd7e2444141d4da08b4c324e7441c64b
shine-lzx <1597395958@qq.com>
2023-03-15 09:40:20 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
 import { NodeSSH } from 'node-ssh'
 import type { ConfigEnv } from 'vite'
 import pc from 'picocolors'
-import Spin from './spin'
+
+class Spin {
+  timer: NodeJS.Timeout | null
+  symbol: string
+  constructor() {
+    this.timer = null
+    this.symbol = '|/-'
+  }
+
+  start(description: string = '正在执行操作') {
+    let index = 0
+    if (this.timer) {
+      clearInterval(Number(this.timer))
+    }
+    this.timer = setInterval(() => {
+      index = ++index % this.symbol.length
+      process.stdout.write(`\r ${this.symbol[index]} ${description}`)
+    }, 500)
+  }
+
+  stop(description: string = '操作完成') {
+    process.stdout.clearLine(0)
+    process.stdout.write('\r')
+    clearInterval(Number(this.timer))
+    console.log(description)
+  }
+}
 
 const loading = new Spin()
 
